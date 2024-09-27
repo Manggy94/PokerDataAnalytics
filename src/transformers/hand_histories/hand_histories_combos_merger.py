43bdf3c0ff28f0cc90ca717808d565441bf94a32
Manggy94 <alex.mangwa@gmail.com>
2024-09-27 13:58:05 +0200

    Correct hh combos merger

--- src/transformers/hand_histories/hand_histories_combos_merger.py
+++ src/transformers/hand_histories/hand_histories_combos_merger.py
@@ -12,6 +12,6 @@ class HandHistoriesCombosMerger(BaseEstimator, TransformerMixin):
 
     def transform(self, X: pd.DataFrame):
         return X\
-            .merge(self.combos, how="left", left_on="hero_combo", right_on="id", suffixes=("", "_hero_combo"))\
+            .merge(self.combos, how="left", left_on="hero_combo", right_on="id", suffixes=("", "_combo"))\
             .drop(columns=["id_combo", "hero_combo"])\
             .rename(columns={c: f"hero_combo_{c}" for c in self.combos.columns if c != "id"})
