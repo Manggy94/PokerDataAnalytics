5fe84d83e6337e5c9958c436e50c3d10568e8589
Manggy94 <alex.mangwa@gmail.com>
2024-10-30 14:52:36 +0100

    Correction de loading des positions

--- src/pipelines/positions.py
+++ src/pipelines/positions.py
@@ -1,9 +1,12 @@
 from sklearn.pipeline import Pipeline
+
+from src.transformers.positions.category_transformer import CategoryTransformer
 from src.transformers.positions.columns_cleaner import PositionsColumnsCleaner
 
 
 class PositionsPipeline(Pipeline):
     def __init__(self):
         super().__init__(steps=[
+            ('category_transformer', CategoryTransformer()),
             ("positions_cleaner", PositionsColumnsCleaner())
         ])
