6b6e5271cb636e07a6525d2bf7b85ebeb1266705
Manggy94 <alex.mangwa@gmail.com>
2024-11-20 19:36:02 +0100

    Update mapper is 2 gapper

--- src/mappers/combos_is_two_gapper.py
+++ src/mappers/combos_is_two_gapper.py
@@ -3,15 +3,15 @@ from src.data.loader import DataLoader
 from pkrcomponents.components.cards.combo import Combo
 
 
-def combos_is_connector_matrix() -> pd.DataFrame:
+def combos_is_two_gapper_matrix() -> pd.DataFrame:
     source = DataLoader().load_combos()
     combos = source.short_name.cat.categories
     matrix = pd.DataFrame(
         index=combos,
-        columns=["is_connector"],
+        columns=["is_two_gapper"],
         data=0,
         dtype=int)
     for combo in Combo:
         x = f"{combo}"
-        matrix.loc[x, "is_connector"] = int(combo.is_connector)
+        matrix.loc[x, "is_two_gapper"] = int(combo.is_two_gapper)
     return matrix
